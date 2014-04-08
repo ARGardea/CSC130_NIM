7a30e278b5cb56eba8f52327dde924d905812097
AGardea <AlexRogerG@gmail.com>
2014-04-08 17:58:06 -0600

    Added Input Processing to the Human Player Class

--- NIM/src/main/BoardState.java
+++ NIM/src/main/BoardState.java
@@ -9,4 +9,17 @@ package main;
  */
 public class BoardState {
 	public int[] rows = new int[3];
+	
+	@Override
+	public String toString(){
+		String result = new String();
+		for(int i = 0; i < rows.length; i++){
+			result += (i + 1) + ") [";
+			for(int j = 0; j < rows[i]; j++){
+				result += " x";
+			}
+			result += " ]\n";
+		}
+		return result;
+	}
 }
--- NIM/src/main/Human.java
+++ NIM/src/main/Human.java
@@ -3,6 +3,8 @@
  */
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author agardea
  *
@@ -13,9 +15,31 @@ public class Human extends Player {
 	 * @see main.Player#takeTurn()
 	 */
 	@Override
-	void takeTurn() {
+	void takeTurn(BoardState currentState) {
 		// TODO Auto-generated method stub
-
+		Boolean inputValid = false;
+		String invalidInputMessage = "That input is invalid.\n Please select a row by entering its number.";
+		while(!inputValid){
+			System.out.println(currentState.toString());
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Select a row: ");
+			String response = sc.next();
+			if(response.length() == 1){
+				try{
+					int r = Integer.parseInt(response);
+					if(r > 0 && r < 4){
+						inputValid = true;
+						System.out.println(response);
+					}else{
+						System.out.println(invalidInputMessage);
+					}
+				}catch(java.lang.NumberFormatException e){
+					System.out.println(invalidInputMessage);
+				}
+			}else{
+				System.out.println(invalidInputMessage);
+			}
+		}
 	}
 
 }
