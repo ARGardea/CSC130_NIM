6461ef5b3cac953b9f301133ec3bec1da6c5aca9
nkagee <nkagee@student.neumont.edu>
2014-04-09 19:44:59 -0600

    Added boardstates to history

--- NIM/src/main/Game.java
+++ NIM/src/main/Game.java
@@ -5,41 +5,41 @@ public class Game
 	Player player1;
 	Player player2;
 	GameBoard board;
+	GameHistory history; 
 	
 	boolean isValidTurn = true;
 	boolean firstPlayerTurn = true;
 	int numberOfTurns = 0;
 	int roundsToPlay = 0;
 	
-	public Game(GameType type)
-	{
-		if(type.equals(GameType.PvP))
-		{
+	public Game(GameType type) {
+		if(type.equals(GameType.PvP)) {
 			startPvPGame();
 		}
-		else if(type.equals(GameType.PvC))
-		{
+		else if(type.equals(GameType.PvC)) {
 			startPvCGame();
 		}
 	}
 	
-	public Game(GameType type, int roundsToPlay)
-	{
+	public Game(GameType type, int roundsToPlay) {
 		startCvCGame(roundsToPlay);
 	}
 	
-	public void startNewTurn(){
+	public void startNewTurn() {
 		numberOfTurns++;
 		printPlayerTurnMessage();
-		if(firstPlayerTurn){
+		if(firstPlayerTurn) {
 			player1.takeTurn(board);
-		}else{
+		} else {
 			player2.takeTurn(board);
 		}
+		
+		history.addState(board.getCurrentState());
+		
 		firstPlayerTurn = !firstPlayerTurn;
 	}
 	
-	public int checkGameStatus(){
+	public int checkGameStatus() {
 		BoardState currentState = board.getCurrentState();
 		int row1 = currentState.rows[0];
 		int row2 = currentState.rows[1];
@@ -51,32 +51,32 @@ public class Game
 		return rowTotal;
 	}
 	
-	public Boolean didFirstPlayerWin(){
+	public Boolean didFirstPlayerWin() {
 		Boolean result = null;
 		int gameStatus = checkGameStatus();
-		if(gameStatus == 0){
+		if(gameStatus == 0) {
 			result = firstPlayerTurn;
-		}else if(gameStatus == 1){
+		} else if(gameStatus == 1) {
 			result = !firstPlayerTurn;
 		}
 		return result;
 	}
 	
-	public void printPlayerVictoryMessage(boolean firstPlayerWin){
+	public void printPlayerVictoryMessage(boolean firstPlayerWin) {
 		String currentPlayerName = "Magnificent Steven";
-		if(firstPlayerWin){
+		if(firstPlayerWin) {
 			currentPlayerName = player1.Name;
-		}else{
+		} else {
 			currentPlayerName = player2.Name;
 		}
 		System.out.println(currentPlayerName + ", you win!\n");
 	}
 	
-	public void printPlayerTurnMessage(){
+	public void printPlayerTurnMessage() {
 		String currentPlayerName = "Magnificent Steven";
 		if(firstPlayerTurn){
 			currentPlayerName = player1.Name;
-		}else{
+		} else {
 			currentPlayerName = player2.Name;
 		}
 		System.out.println(currentPlayerName + ", it's your turn!");
@@ -98,34 +98,40 @@ public class Game
 		}
 	}
 	
-	public void startPvPGame()
-	{
+	public void startPvPGame() {
 		numberOfTurns = 0;
 		roundsToPlay = 1;
+		history = new GameHistory();
 		board = new GameBoard();
 		player1 = new Human("Player 1");
 		player2 = new Human("Player 2");
+		
+		history.addState(board.getCurrentState());
 	}
 	
-	public void startPvCGame()
-	{
+	public void startPvCGame() {
 		numberOfTurns = 0;
 		roundsToPlay = 1;
+		history = new GameHistory();
 		board = new GameBoard();
 		player1 = new Human("Player");
 		player2 = new Computer("Computer");
+		
+		history.addState(board.getCurrentState());
 	}
 	
-	public void startCvCGame(int roundsToPlay)
-	{
+	public void startCvCGame(int roundsToPlay) {
 		this.roundsToPlay = roundsToPlay;
 		numberOfTurns = 0;
 		for(int i = 0; i < roundsToPlay; i++)
 		{
 			System.out.println(roundsToPlay);
 		}
+		history = new GameHistory();
 		board = new GameBoard();
 		player1 = new Computer("Computer 1");
 		player2 = new Computer("Computer 2");
+		
+		history.addState(board.getCurrentState());
 	}
 }
