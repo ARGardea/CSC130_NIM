538069b50c71adbecad7cbfa74125b040321d48b
AGardea <AlexRogerG@gmail.com>
2014-04-15 19:22:57 -0600

    Renamed HistoryEvaluator to TurnEvaluator

--- NIM/src/main/TurnEvaluator.java
+++ NIM/src/main/TurnEvaluator.java
@@ -2,15 +2,19 @@ package main;
 
 import java.util.ArrayList;
 
-public class HistoryEvaluator
+public class TurnEvaluator
 {
 	private ArrayList<GameHistory> historyList; 
 	
-	public HistoryEvaluator() {
+	public TurnEvaluator() {
 		historyList = new ArrayList<GameHistory>();
 	}
 	
 	public ArrayList<GameHistory> getHistoryList() {
 		return historyList;
+	}
+	
+	public void EvaluateHistory(GameHistory historyToEvaluate){
+		historyList.add(historyToEvaluate);
 	}
 }
